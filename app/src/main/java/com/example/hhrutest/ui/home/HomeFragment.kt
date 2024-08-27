52790d04e3eed99ce997008a5bdd69f7513bbe36
robarcoo <111498279+robarcoo@users.noreply.github.com>
2024-08-27 22:46:23 +0300

    first two screes are ready

--- app/src/main/java/com/example/hhrutest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hhrutest/ui/home/HomeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.hhrutest.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
@@ -32,16 +37,52 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         binding.loginButton.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("login", binding.email.text.toString())
             val navHostFragment = (activity as AppCompatActivity)
                 .supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
             val navController = navHostFragment.navController
-            navController.navigate(R.id.codeFragment)
+            navController.navigate(R.id.codeFragment, bundle)
         }
+
+        binding.email.addTextChangedListener(textWatcher)
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val button = binding.loginButton
+            if (s != null) {
+                if (s.isEmpty()) {
+                    button.isEnabled = false
+                    binding.loginEditText.isErrorEnabled = false
+                    binding.loginEditText.setBoxStrokeColorStateList(AppCompatResources.getColorStateList(context!!, R.color.grey_34))
+                    button.setBackgroundResource(R.drawable.buttonshape)
+                    button.setTextColor(ContextCompat.getColor(context!!, R.color.grey_9f))
+                } else if (android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+                    binding.loginEditText.isErrorEnabled = false
+                    binding.loginEditText.setBoxStrokeColorStateList(AppCompatResources.getColorStateList(context!!, R.color.grey_34))
+                    binding.loginButton.isEnabled = true
+                    button.setBackgroundResource(R.drawable.button_shape_enabled)
+                    button.setTextColor(ContextCompat.getColor(context!!, R.color.white))
+                } else {
+                    binding.loginEditText.isErrorEnabled = true
+                    binding.loginEditText.setBoxStrokeColorStateList(AppCompatResources.getColorStateList(context!!, R.color.red))
+                    binding.loginEditText.error = "Невалидный логин"
+                    binding.loginButton.isEnabled = false
+                    button.setBackgroundResource(R.drawable.buttonshape)
+                    button.setTextColor(ContextCompat.getColor(context!!, R.color.grey_9f))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/hhrutest/ui/login/CodeFragment.kt
+++ app/src/main/java/com/example/hhrutest/ui/login/CodeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.hhrutest.ui.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
@@ -31,8 +35,38 @@ class CodeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.sentCodeTitle.text = "Отправили код на ${arguments?.getString("login")}"
+        binding.otp.addTextChangedListener(textWatcher)
+        binding.codeButton.setOnClickListener {
+            val navHostFragment = (activity as AppCompatActivity)
+                .supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.navigation_dashboard)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        val button = binding.codeButton
+        if (s != null) {
+            if (s.length == 4) {
+                button.isEnabled = true
+                button.setBackgroundResource(R.drawable.button_shape_enabled)
+                button.setTextColor(ContextCompat.getColor(context!!, R.color.white))
+            } else {
+                button.isEnabled = false
+                button.setBackgroundResource(R.drawable.buttonshape)
+                button.setTextColor(ContextCompat.getColor(context!!, R.color.grey_9f))
+            }
+        }
+    }
+}
 }
